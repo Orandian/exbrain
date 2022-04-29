47762231aa3d633f69c7412a528de076207609da
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-04-29 16:19:18 +0630

    29.4.2022

--- advance/homework/HW5/app.js
+++ advance/homework/HW5/app.js
@@ -2,8 +2,10 @@ var display = document.getElementById('display');
 var userInput = document.getElementById('userInput');
 var capLock = document.getElementById('caplock');
 
+// to add keyword in the display
 function typing(e) {
   var text = '';
+  // to check Caplock is open or not
   if (capLock.checked == true) {
     text = e.value.toUpperCase();
   } else {
@@ -12,55 +14,63 @@ function typing(e) {
   display.innerHTML += text;
 }
 
+// to clear the display and input box
 function blank() {
   display.innerHTML = '';
   userInput.value = '';
 }
 
+// charAt function
 function charat() {
   window.alert(display.innerHTML.charAt(userInput.value));
 }
 
+// charCodeAt function
 function charcodeat() {
   window.alert(display.innerHTML.charCodeAt(userInput.value));
 }
 
+// toUpperCase function
 function touppercase() {
   window.alert(display.innerHTML.toUpperCase());
 }
 
+// toLowerCase function
 function tolowercase() {
   window.alert(display.innerHTML.toLowerCase());
 }
 
+// trim function
 function trim() {
   window.alert(display.innerHTML.trim());
 }
 
+// includes function
 function includes() {
   window.alert(display.innerHTML.includes(userInput.value));
 }
 
+// indexOf function
 function indexof() {
   window.alert(display.innerHTML.indexOf(userInput.value));
 }
 
+// lastIndexOf function
 function lastindexof() {
   window.alert(display.innerHTML.lastIndexOf(userInput.value));
 }
 
+// startsWith function
 function startswith() {
   window.alert(display.innerHTML.startsWith(userInput.value));
 }
 
+// endsWith function
 function endswith() {
   window.alert(display.innerHTML.endsWith(userInput.value));
 }
 
-function clear() {
-  display.innerHTML = '';
-}
-
+// if press wrong key
 function invalid() {
   window.alert('Invalid Key');
 }
