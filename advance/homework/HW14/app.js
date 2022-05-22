5309dbf704d8295944d3159bb20b6e8e75da6b01
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-05-22 19:50:38 +0630

    22.5.2022

---
+++ advance/homework/HW14/app.js
@@ -0,0 +1,41 @@
+const question = document.getElementById('question');
+const addNumbers = document.getElementById('addNumbers');
+const result = document.getElementById('result');
+const getNumbers = document.getElementsByClassName('getNumber');
+let answer = 0;
+let randomNumber = 0;
+
+function random() {
+  randomNumber = Math.floor(Math.random() * 100 + 1);
+  question.innerHTML = randomNumber;
+}
+random();
+
+function getNumber(e) {
+  let value = +e.innerHTML;
+  answer += value;
+  if (addNumbers.innerHTML == '') {
+    addNumbers.innerHTML += value;
+  } else {
+    addNumbers.innerHTML += '+' + value;
+  }
+  result.innerHTML = answer;
+
+  if (randomNumber == answer) {
+    document.getElementById('tryResult').innerHTML = 'You Win!';
+    for (let i = 0; i < getNumbers.length; i++) {
+      getNumbers[i].onclick = null;
+    }
+  }
+
+  if (answer > randomNumber) {
+    document.getElementById('tryResult').innerHTML = 'You Lose!';
+    for (let i = 0; i < getNumbers.length; i++) {
+      getNumbers[i].onclick = null;
+    }
+  }
+}
+
+function again() {
+  window.location.reload();
+}
