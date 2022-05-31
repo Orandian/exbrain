11a3696065aecc0f7599eb5b80ee7e1c6369baf2
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-05-31 17:51:42 +0630

    31.5.2022

--- advance/homework/HW15/js/app.js
+++ advance/homework/HW15/js/app.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
   $('#addToCart').click(function () {
     $('#itemList').append(`
         <div class="addItem">
-            <div class="icon"><ion-icon name="trash-bin-outline" id="trash"></ion-icon></div>
-            <img src="./img/bag.png" alt="bag" />
-            <p class="price">Price: $<span>100</span></p>
-            <p class="quantity">Quantity: ${$('#quantity').val()}</p>
+          <div class="icon"><ion-icon name="trash-bin-outline" class="trash"></ion-icon></div>
+          <h1>${$('#name').val()}</h1>
+          <img src="./img/bag.png" alt="bag" />
+          <p class="price">Price: $<span>${$('#price').val()}</span></p>
+          <p class="quantity">Quantity: ${$('#quantity').val()}</p>
         </div>
     `);
-    $('#trash').click(function () {
+    $('.trash').click(function () {
       $(this).parents('.addItem').remove();
     });
   });
