cdf9c2ce90c4e3d1c3996b50597a16c5eba8e418
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-06-07 18:14:20 +0630

    7.6.2022

--- advance/homework/HW16/script.js
+++ advance/homework/HW16/script.js
@@ -1,169 +1,264 @@
-// $(document).ready(function () {
-//   var amount = 0;
-//   //   $('.cart').hide();
+// $('document').ready(function () {
+//   $('.cart').hide();
 //   $('#discounttitle').hide();
 
-//   let date = new Date().getDay();
-//   if (date == 7 || date == 6) {
-//     $('#discounttitle').show();
-//   }
-//   $('.card').click(function () {
-//     // $('.cart').slideDown('slow');
-//     var eachPrice = +$(this).find('.priceValue').text();
-//     amount += +$(this).find('.priceValue').text();
-//     if (date == 7 || date == 6) {
-//       amount = parseInt(amount - (+amount * 15) / 100);
-//     }
+//   init();
+// });
 
-//     $('#discountprice').text(`
-//         ${amount} kyats
-//     `);
+// function init() {
+//   var total = 0;
+
+//   $('.card').click(function () {
+//     var delivery = 0;
+//     var quantityValue = 0;
+//     var grandTotal = 0;
+//     delivery = +$('#delivery').val();
+//     $('.cart').slideDown('slow');
+//     var eachPrice = +$(this).find('.priceValue').text();
 
 //     $('.calculateitem').append(`
-//         <div class="item" value="${eachPrice}">
-//             <img src="${$(this).find('img').attr('src')}"/>
-//             <div class="details">
-//                 <p>${$(this).find('.pname').text()}</p>
-//                 <p>(${$(this).find('.code').text()})</p>
-//             </div>
-//             <input type="text" class="quantity" value="1"/>
-//             <ion-icon name="trash-outline" class="delete"></ion-icon>
-//         </div>
-//     `);
+//           <div class="item" value="${eachPrice}">
+//               <img src="${$(this).find('img').attr('src')}"/>
+//               <div class="details">
+//                   <p>${$(this).find('.pname').text()}</p>
+//                   <p>(${$(this).find('.code').text()})</p>
+//               </div>
+//               <input type="text" class="quantity" value="1"/>
+//               <ion-icon name="trash-outline" class="delete"></ion-icon>
+//           </div>
+//       `);
+
+//     total += eachPrice;
+
+//     $('#delivery').click(function () {
+//       delivery = +$(this).val();
+//       grandTotal = total + quantityValue + delivery;
+
+//       $('#grand').text(`
+//           ${grandTotal} kyats
+//         `);
+//     });
 
 //     $('.quantity').keyup(function () {
-//       if ($('.quantity').val() > 9) {
-//         window.alert('error');
+//       var inputValue = +$(this).val();
+//       quantityValue =
+//         +$(this).parents('.item').attr('value') * (inputValue - 1);
+//       if (inputValue > 9) {
+//         window.alert('Error');
+//         inputValue = 1;
+//         $(this).val(inputValue);
 //       } else {
-//         eachPrice =
-//           +$(this).parents('.item').attr('value') * (+$(this).val() - 1);
+//         grandTotal = total + delivery + quantityValue;
 
-//         $('#discountprice').text(`
-//         ${amount + eachPrice} kyats
-//       `);
+//         $('#grand').text(`
+//             ${grandTotal} kyats
+//           `);
 //       }
 //     });
 
 //     $('.quantity').blur(function () {
-//       var inputValue = $('.quantity').val();
-//       if (inputValue == '' || inputValue >= 10) {
-//         inputValue = 1;
-//         $('.quantity').val(inputValue);
+//       if ($(this).val() < 1 || $(this).val() > 9) {
+//         $(this).val('1');
+//         window.alert('error');
 //       }
 //     });
 
-//     $('.delete').click(function () {
+//     var date = new Date().getDay();
+//     if (date == 6 || date == 0) {
+//       $('#discounttitle').show();
+//       var percentage = (total * 15) / 100;
+//       total = parseInt(total - percentage);
 //       $('#discountprice').text(`
-//         ${amount - eachPrice} kyats
-//       `);
+//         ${total} kyats
+//         `);
+//     }
+//     grandTotal = total;
+
+//     $('.delete').click(function () {
+//       // $('#grand').text(`
+//       //         ${grandTotal - eachPrice} kyats
+//       // `);
 //       $(this).parents('.item').remove();
 //     });
+
+//     $('#grand').text(`
+//         ${grandTotal + delivery} kyats
+//       `);
 //   });
-// });
 
-$('document').ready(function () {
-  // $('.cart').hide();
-  var date = new Date().getDay();
-  if (date == 6 || date == 7) {
-    $('#discounttitle').show();
-  }
+//   $('#order').click(function () {
+//     $('#orderdetail').html(`
+//         Thank you ${$('#yourname').val()} <br/>
+//         We received your order! <br/>
+//         We will Deliver to your place at ${$('#address').val()} <br/>
+//         Before delivery, we will inform to you Phone: ${$('#phone').val()}
+//       `);
+//   });
+// }
 
-  init();
-});
+// Global Variables
+let today = new Date();
+let buyItem_price = [];
+let quantity = [];
+let itemtotal = [];
+let discount = false;
+let count = 0;
+let perQuantity;
+let totalprice = 0;
 
-function init() {
-  var total = 0;
-  var click = false;
+$(document).ready(function () {
+  initial();
 
-  if (click == false) {
-    $('.card').click(function () {
-      var delivery = 0;
-      var quantityValue = 0;
-      var grandTotal = 0;
-      delivery = +$('#delivery').val();
-      // $('.cart').slideDown('slow');
-      var eachPrice = +$(this).find('.priceValue').text();
+  $('.card').click(function () {
+    // slide down to cart
+    $('.cart').slideDown(1500);
 
-      $('.calculateitem').append(`
-          <div class="item" value="${eachPrice}">
-              <img src="${$(this).find('img').attr('src')}"/>
-              <div class="details">
-                  <p>${$(this).find('.pname').text()}</p>
-                  <p>(${$(this).find('.code').text()})</p>
-              </div>
-              <input type="text" class="quantity" value="1"/>
-              <ion-icon name="trash-outline" class="delete"></ion-icon>
-          </div>
-      `);
+    // show Order button
+    $('#order').show();
 
-      total += eachPrice;
+    // check already add or not
+    let alreadyExist = false;
+    let img = $(this).find('img').attr('src');
+    let pname = $(this).find('.pname').text();
+    let code = $(this).find('.code').text();
+    let price = $(this).find('.price').text();
 
-      $('#delivery').click(function () {
-        delivery = +$(this).val();
-        grandTotal = total + quantityValue + delivery;
+    let items = $('.item');
+    for (let index = 0; index < items.length; index++) {
+      let exist = items[index].childNodes[1].childNodes[1].innerText;
 
+      if (exist == code) {
+        alreadyExist = true;
+        alert('Item has already in cart!');
+      }
+    }
+
+    if (!alreadyExist) {
+      // add price for each item
+      buyItem_price.push(Number(price.replace('Ks ', '')));
+
+      totalprice = +buyItem_price.reduce((a, b) => a + b, 0);
+      // console.log(totalprice);
+
+      if (discount) {
+        let percentage = Math.floor((totalprice / 100) * 15);
+
+        totalprice -= percentage;
+
+        $('#discountprice').text(`${totalprice} kyats`);
+      }
+
+      $('.calculateitem').append(
+        '<div class="item" value="' +
+          price +
+          '">' +
+          '<img src="' +
+          img +
+          '" alt=""/>' +
+          '<div class="itemdetail">' +
+          '<p id="pname">' +
+          pname +
+          '</p>' +
+          '<p id="code">' +
+          code +
+          '</p>' +
+          '</div>' +
+          '<input type="text" value="1" class="quantity"/>' +
+          '<div id="' +
+          count +
+          '" class="delete"><ion-icon name="trash-outline"></ion-icon></div>' +
+          '</div>'
+      );
+    }
+
+    $('.quantity').keyup(function () {
+      let itemQuantity = +$(this).val();
+      let itemValue = +$(this)
+        .parents('.item')
+        .attr('value')
+        .replace('Ks ', '');
+      if (itemQuantity > 9) {
+        window.alert('Error');
+        $(this).val('1');
+      } else {
+        let quantityResult = itemValue * itemQuantity - itemValue;
+        let grand = +$('#grand').text().replace(' kyats', '');
+        // console.log(grand + quantityResult);
         $('#grand').text(`
-          ${grandTotal} kyats
-        `);
-      });
-
-      $('.quantity').keyup(function () {
-        var inputValue = +$(this).val();
-        quantityValue =
-          +$(this).parents('.item').attr('value') * (inputValue - 1);
-        if (inputValue > 9) {
-          window.alert('Error');
-          inputValue = 1;
-          $(this).val(inputValue);
-        } else {
-          grandTotal = total + delivery + quantityValue;
-
-          $('#grand').text(`
-            ${grandTotal} kyats
-          `);
-        }
-      });
-
-      $('.quantity').blur(function () {
-        if ($(this).val() < 1 || $(this).val() > 9) {
-          $(this).val('1');
-          window.alert('error');
-        }
-      });
-
-      var date = new Date().getDay();
-      if (date == 6 || date == 7) {
-        $('#discounttitle').show();
-        var percentage = (total * 15) / 100;
-        total = parseInt(total - percentage);
-        $('#discountprice').text(`
-        ${total} kyats
+          ${grand + quantityResult} kyats
         `);
       }
-      grandTotal = total;
+    });
 
-      $('.delete').click(function () {
-        // $('#discountprice').text(`
-        //         ${amount - eachPrice} kyats
-        //       `);
-        $(this).parents('.item').remove();
-      });
+    $('.quantity').blur(function () {
+      if ($(this).val() <= 0 || $(this).val() > 9) {
+        $(this).val('1');
+        let itemValue = +$(this)
+          .parents('.item')
+          .attr('value')
+          .replace('Ks ', '');
+        let grand = +$('#grand').text().replace(' kyats', '');
+        $('#grand').text(`
+          ${grand + itemValue} kyats
+        `);
+      }
+    });
 
+    $('.delete').click(function () {
+      let itemQuantity = +$(this).siblings('.quantity').val();
+      let itemValue = +$(this)
+        .parents('.item')
+        .attr('value')
+        .replace('Ks ', '');
+      let grand = +$('#grand').text().replace(' kyats', '');
+      // console.log(grand + quantityResult);
       $('#grand').text(`
-        ${grandTotal + delivery} kyats
-      `);
+          ${grand - itemValue * itemQuantity} kyats
+        `);
+      $(this).parents('.item').remove();
     });
 
-    $('#order').click(function () {
-      $('#orderdetail').html(`
-        Thank you ${$('#yourname').val()} <br/>
-        We received your order! <br/>
-        We will Deliver to your place at ${$('#address').val()} <br/>
-        Before delivery, we will inform to you Phone: ${$('#phone').val()}
-      `);
-    });
-    click = true;
-  } else {
-    click = false;
+    $('#grand').text(`${totalprice + 3000} kyats`);
+  });
+
+  $('#delivery').click(function () {
+    let delivery = +$(this).val();
+    $('#grand').text(`${totalprice + delivery} kyats`);
+  });
+
+  $('#order').click(function () {
+    $('#orderdetail').html(`
+      Thank you ${$('#yourname').val()} <br/>
+      We received your order! <br/>
+      We will Deliver to your place at ${$('#address').val()} <br/>
+      Before delivery, we will inform to you Phone: ${$('#phone').val()}
+    `);
+  });
+});
+
+// this function is used for initial states
+function initial() {
+  $('#discounttitle').hide();
+  $('#discountprice').hide();
+  $('#order').hide();
+  $('#orderdetails').hide();
+  $('.cart').hide();
+
+  // call weekend or not function
+  checkWeekend();
+}
+
+function checkWeekend() {
+  // get today date
+  // sunday : 0 , Sat : 6
+  if (today.getDay() == 0 || today.getDay() == 6) {
+    // check time between 9AM and 6PM
+    if (today.getHours() >= 9 && today.getHours() <= 18) {
+      $('#discounttitle').show();
+      $('#discountprice').show();
+
+      // set discount flag to true
+      discount = true;
+    }
   }
 }
