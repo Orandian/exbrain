5ab077fb1b19cc6b1821142b2d41c552ad2bd267
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-06-15 00:21:02 +0630

    15.6.2022

--- advance/homework/HW20/app.js
+++ advance/homework/HW20/app.js
@@ -1,7 +1,20 @@
 const toggle = document.getElementById("toggle");
 const html = document.querySelector("html");
+const save = document.getElementById("save");
+
+let name = document.getElementById("name");
+let position = document.getElementById("position");
+let email = document.getElementById("email");
+let address = document.getElementById("address");
+let phone = document.getElementById("phone");
 
 function toggleDarkMode() {
+  name.value = localStorage.getItem("name");
+  position.value = localStorage.getItem("position");
+  email.value = localStorage.getItem("email");
+  address.value = localStorage.getItem("address");
+  phone.value = localStorage.getItem("phone");
+
   if (localStorage.toggleDarkMode == "") {
     toggle.checked = true;
     html.classList.add("dark");
@@ -22,4 +35,12 @@ toggle.addEventListener("click", () => {
     localStorage.toggleDarkMode = "";
     toggle.checked = true;
   }
+});
+
+save.addEventListener("click", () => {
+  localStorage.setItem("name", name.value);
+  localStorage.setItem("position", position.value);
+  localStorage.setItem("email", email.value);
+  localStorage.setItem("address", address.value);
+  localStorage.setItem("phone", phone.value);
 });
